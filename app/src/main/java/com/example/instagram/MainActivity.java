9dfcd39ba43625b7241c22140c8d077c0c607400
jennhe <jenhe2000@gmail.com>
2019-07-09 12:50:40 -0700

    added signup and persisted login functionality

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -11,31 +11,54 @@ import android.widget.EditText;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText usernameInput;
     private EditText passwordInput;
     private Button loginBtn;
+    private Button signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        usernameInput = findViewById(R.id.usernameInput);
-        passwordInput = findViewById(R.id.passwordInput);
-        loginBtn = findViewById(R.id.loginBtn);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            setContentView(R.layout.activity_main);
 
-        loginBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                final String username = usernameInput.getText().toString();
-                final String password = passwordInput.getText().toString();
+            usernameInput = findViewById(R.id.usernameInput);
+            passwordInput = findViewById(R.id.passwordInput);
+            loginBtn = findViewById(R.id.loginBtn);
+            signupBtn = findViewById(R.id.signupBtn);
 
-                login(username, password);
-            }
-        });
+
+            loginBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    final String username = usernameInput.getText().toString();
+                    final String password = passwordInput.getText().toString();
+
+                    login(username, password);
+                }
+            });
+
+            signupBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    final String username = usernameInput.getText().toString();
+                    final String password = passwordInput.getText().toString();
+
+                    signup(username, password);
+                }
+            });
+        }
+
     }
 
     private void login(String username, String password){
@@ -50,6 +73,33 @@ public class MainActivity extends AppCompatActivity {
                     finish();
                 } else {
                     Log.d("LoginActivity", "Login failure.");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void signup(final String username, final String password){
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("SignupActivity", "Signup successful!");
+
+//                    login(username, password);
+
+//                    final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+//                    startActivity(intent);
+//                    finish();
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                    Log.d("SignupActivity", "Signup failure", e);
                     e.printStackTrace();
                 }
             }
